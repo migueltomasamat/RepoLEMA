a735bb19c246577e238f1199b38d9b2edba258ed
Miguel Ángel Tomás Amat <migueltomas.informatica@iespacomolla.es>
2023-02-23 13:39:29 +0100

    Añadida la funcionalidad del acumulador

--- CalculadoraJavascript/js/funciones.js
+++ CalculadoraJavascript/js/funciones.js
@@ -1,7 +1,22 @@
 let arrayNumeros=[];
+let acumulador=0;
+
+document.querySelector('.primeraFila:first-child').addEventListener('dblclick', function(){
+    acumulador=0;
+    arrayNumeros=[];
+});
+
+function acumular(){
+    acumulador+=parseInt(document.querySelector('input').value);
+    document.querySelector('input').value='0';
+}
+
+function resultadoAcumulardor(){
+    document.querySelector('input').value=acumulador.toString();
+}
 
 function mostrarValorEnInput(valor){
-    if (document.querySelector('input').value==0){
+    if (document.querySelector('input').value==='0'){
         document.querySelector('input').value=valor;
     }else{
         document.querySelector('input').value+=valor;
@@ -49,7 +64,7 @@ function multiplicacion(){
 }
 function division(){
     const denominador=parseInt(arrayNumeros.pop());
-    if(denominador==0){
+    if(denominador===0){
         document.querySelector('input').value='Errr';
     }else{
         const numerador=parseInt(arrayNumeros.pop());
@@ -61,7 +76,32 @@ function division(){
 }
 
 function borrarInput(){
-    document.querySelector('input').value=0;
+    let pantalla= document.querySelector('input').value;
+    //Encontramos donde se situa el operador
+    let pos_operador=pantalla.indexOf(obtenerOperador(pantalla));
+    console.log(pos_operador);
+    if (pos_operador>0){
+        document.querySelector('input').value=pantalla.slice(0,pos_operador);
+    }
+    else{
+        console.log('Ha borrado');
+        document.querySelector('input').value='0';
+    }
+}
+
+function obtenerOperador(cadena){
+    if(cadena.includes('+')){
+        return '+';
+    }
+    if (cadena.includes('-')){
+        return '-';
+    }
+    if (cadena.includes('*')){
+        return '*';
+    }
+    if (cadena.includes('/')){
+        return '/';
+    }
 }
 
 function total(){
